0b017a023cab78cbae844e40b27cd7a7c45d0755
milisarge <milisarge@gmail.com>
2023-11-01 08:44:53 +0300

    alsa-servis

---
+++ ayarlar/init/alsa.lua
@@ -0,0 +1,15 @@
+-- alsa restore
+-- save and restore alsa state
+
+local task={
+	desc="to restore alsasound",
+			
+	start={
+		cmd="alsactl restore",
+	},
+	stop={
+		cmd="alsactl save",
+	},
+	
+}
+return task
