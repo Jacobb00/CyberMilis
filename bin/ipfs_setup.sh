42cff76cc153abcd850036b802573e41242136ac
milisarge <milisarge@gmail.com>
2023-11-20 16:24:22 +0300

    ipfs

---
+++ bin/ipfs_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Milis Linux Hızlı IPFS Kurucu 2023
+[ -z $1 ] && exit 1
+
+version="0.24.0"
+mkdir -p $HOME/{.cache,.local}
+cd $HOME/.cache
+
+if [ ! -f kubo_v${version}_linux-amd64.tar.gz ];then
+  wget https://dist.ipfs.tech/kubo/v${version}/kubo_v${version}_linux-amd64.tar.gz
+fi
+
+tar -xvzf kubo_v${version}_linux-amd64.tar.gz
+cd kubo
+cp -f ipfs /usr/local/bin/
+chmod +x /usr/local/bin/ipfs
+cd ..; rm -rf kubo
+
+# ipfs init-daemon
+
