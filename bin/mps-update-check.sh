c0580c72d6bf64e1df9b719fc3bc70fe32fde876
milisarge <milisarge@gmail.com>
2023-12-14 16:26:14 +0300

    mps-update

---
+++ bin/mps-update-check.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+milis="/sources/gitlab.com/milislinux.milis23.git"
+if [ -d $milis ];then
+milisj="{\"milis\":\"güncel değil\"}"
+
+wid=`curl -s https://gitlab.com/api/v4/projects/51691781/repository/commits?per_page=1 | jq -r .[].id`
+
+cd $milis
+lid=`git log --pretty=oneline | head -n1| awk '{print $1}'`
+
+[ "$lid" == "$wid" ] && milisj="{\"milis\":\"güncel\"}"
+else
+milisj="{\"milis\":\"dizin yok\"}"
+fi
+mps="/usr/milis/mps"
+mpsj="{\"mps\":\"güncel değil\"}"
+
+wid=`curl -s https://gitlab.com/api/v4/projects/51691929/repository/commits?per_page=1 | jq -r .[].id`
+
+cd $mps
+lid=`git log --pretty=oneline | head -n1| awk '{print $1}'`
+
+[ "$lid" == "$wid" ] && mpsj="{\"mps\":\"güncel\"}"
+
+echo "[$milisj,$mpsj]"
