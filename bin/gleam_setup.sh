ac6f14046cdac454fa0dea3421d7cdc9aa29b324
milisarge <milisarge@gmail.com>
2024-01-25 10:32:37 +0300

    gleam_setup.sh

--- bin/gleam_setup.sh
+++ bin/gleam_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # gleam programming language installer
-[ ! -d /var/lib/mps/db/erlang ] && mps kur erlang
+[ ! -d /var/lib/mps/db/erlang ] && sudo mps kur erlang
 version="0.34.1"
 file="gleam-v${version}-x86_64-unknown-linux-musl.tar.gz"
 cd ~/.cache
 if [ ! -f ${file} ];then
   wget https://github.com/gleam-lang/gleam/releases/download/v${version}/${file}
 fi
-tar xf $file -C /usr/local/bin/
-sudo mv gleam /usr/local/bin/gleam
+tar xf $file
+chmod +x gleam
+sudo mv -v gleam /usr/local/bin/gleam
