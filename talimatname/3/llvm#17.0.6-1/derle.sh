7998f2f06fa2526cd20056c4f0a3dafb53cf103a
milisarge <milisarge@gmail.com>
2024-03-26 09:23:14 +0300

    llvm-ayri-3

--- talimatname/3/llvm#17.0.6-1/derle.sh
+++ talimatname/3/llvm#17.0.6-1/derle.sh
@@ -4,14 +4,21 @@ export CXX=g++
 export CFLAGS=${CFLAGS/-g /-g1 }
 export CXXFLAGS=${CXXFLAGS/-g /-g1 }
 
+cd $SRC
+
+rename -v -- "-$surum.src" '' {cmake,third-party}-$surum.src
+
 cd $isim-$surum.src
 
 mkdir build
 cd build
 
-cmake .. -G Ninja \	
+cmake .. -G Ninja \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=/usr \
+-DLLVM_INSTALL_UTILS=ON \
+-DLLVM_LINK_LLVM_DYLIB=ON \
+-DLLVM_USE_PERF=ON \
 -DLLVM_HOST_TRIPLE=$CHOST \
 -DLLVM_BUILD_LLVM_DYLIB=ON \
 -DLLVM_LINK_LLVM_DYLIB=ON \
@@ -24,7 +31,6 @@ cmake .. -G Ninja \
 -DLLVM_ENABLE_SPHINX=OFF \
 -DLLVM_ENABLE_DOXYGEN=OFF \
 -DLLVM_ENABLE_OCAMLDOC=OFF \
--DSPHINX_WARNINGS_AS_ERRORS=OFF \
 -DFFI_INCLUDE_DIR=$(pkg-config --variable=includedir libffi) \
 -DLLVM_BINUTILS_INCDIR=/usr/include
 ninja all
