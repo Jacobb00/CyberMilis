13f372b519f33c1711fc694eebdd3d3f5fd03565
milisarge <milisarge@gmail.com>
2024-04-29 10:43:32 +0300

    oto-disk-lvm-kontrol

--- bin/oto-diskpart.sh
+++ bin/oto-diskpart.sh
@@ -12,6 +12,12 @@ if [ ! -z $disk ];then
     echo -e ",100M,EF\n;" | sfdisk $disk
     echo "$disk 2 parça ayrılacak." >> $log_file
     mkfs.vfat -F32 ${disk}1 || echo "${disk}1 formatlanamadı." >> $log_file
+    # lvm kontrol
+    lvs -o +devices --no-headings | grep ${disk}2
+    if [ $? -eq 0 ];then
+      vgchange -an `lvs -o +devices --no-headings | grep ${disk}2 | awk '{print $2}'`
+      echo "${disk}2 lvm pasif edildi." >> $log_file
+    fi
     mkfs.ext4 -F ${disk}2 || echo "${disk}2 formatlanamadı." >> $log_file
     echo "$disk 2 parça ayrıldı." >> $log_file
   else
@@ -26,4 +32,3 @@ fi
 
 echo "`blkid | grep $disk`" >> $log_file
 echo "$(date) otodisk bitti" >> $log_file
-
