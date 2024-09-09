3cabffd012e745c6162dfd2344577119a5105c81
milisarge <milisarge@gmail.com>
2024-09-09 12:54:05 +0300

    firefox

---
+++ talimatname/3/firefox/firefox#124.0.2-1/firefox-vendor.js
@@ -0,0 +1,5 @@
+pref("intl.locale.requested", "");
+pref("browser.shell.checkDefaultBrowser", false);
+pref("extensions.autoDisableScopes", 11);
+pref("extensions.shownSelectionUI", true);
+pref("browser.tabs.inTitlebar", 0); 
