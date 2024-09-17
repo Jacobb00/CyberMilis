eb00d0e9f3126955e1d718942d18e88074578c4b
milisarge <milisarge@gmail.com>
2024-09-17 13:43:33 +0300

    ada_setup

---
+++ bin/ada_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Milis Linux Hızlı Go Derleyici Kurucu 2022
+[ -z $1 ] && exit 1
+
+# 2.0.1
+version=$1
+file="alr-${version}-bin-x86_64-linux.zip"
+echo $file
+mkdir -p $HOME/{.cache,.local}
+cd $HOME/.cache
+[ ! -f ${file} ] && wget https://github.com/alire-project/alire/releases/download/v${version}/${file}
+
+rm -rf $HOME/.local/alire && \
+mkdir -p $HOME/.local/alire && \
+cd $HOME/.local/alire && unzip $HOME/.cache/${file}
+
+echo 'export PATH=$PATH:$HOME/.local/alire/bin' >> ~/.bashrc
