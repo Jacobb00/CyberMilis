0353f294aafddd49aa4dcd9c45262a2fbcab397c
milisarge <milisarge@gmail.com>
2024-09-28 16:17:40 +0300

    ygdrasil-init

--- ayarlar/init/yggdrasil.lua
+++ ayarlar/init/yggdrasil.lua
@@ -3,7 +3,7 @@ local task={
 	desc="Yggdrasil Server",
 	program="yggdrasil",
 	start={
-	    cmd="yggdrasil -useconffile /etc/yggdrasil.conf",
+	    cmd="yggdrasil -useconffile /etc/yggdrasil.conf 2>&1> /var/log/yggdrasil.log &",
 	},
 	stop={type="program"},
 	status={type="program"}
