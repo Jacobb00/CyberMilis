5d7bed0afbdb74b9661ed6f7de9f7863e2c6f4dc
milisarge <milisarge@gmail.com>
2024-10-07 13:26:20 +0300

    gtk4

--- talimatname/2/gnome/gtk4#4.12.3-1/kurkos.sh
+++
@@ -1,2 +0,0 @@
-gtk-query-immodules-4.0 --update-cache
-glib-compile-schemas /usr/share/glib-2.0/schemas
--- talimatname/2/gnome/gtk4#4.12.3-1/otokos.sh
+++
@@ -1,6 +0,0 @@
-# glib için schema içeren paketlerin otomatik şema derleme işlemidir.
-kur=1
-sil=1
-
-kontrol="ls usr/lib/gtk-4.0/4.0.0/immodules/*.so"
-betik="/usr/bin/gtk-query-immodules-4.0 --update-cache"
