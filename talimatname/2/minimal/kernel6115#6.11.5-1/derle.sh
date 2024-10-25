9c35e8c1a2b0295c1ab7364c7f060f70e933c6b4
milisarge <milisarge@gmail.com>
2024-10-25 13:48:48 +0300

    kernel6115

---
+++ talimatname/2/minimal/kernel6115#6.11.5-1/derle.sh
@@ -0,0 +1,24 @@
+  set -e
+  # derleme ortamı için
+  ulimit -n 12000
+  bsurum=6.11.5
+  cd "${SRC}/linux-${bsurum}"
+
+  # standart config
+  cat "${SRC}/kernel-${bsurum}-config" > ./.config
+  
+  # 6.11.5 archlinux patch
+  #patch -Np1 < $TDIR/linux-v6.7.8.patch
+  
+  export KBUILD_BUILD_USER="milisarge"
+  export KBUILD_BUILD_HOST="mls.akdeniz.edu.tr"
+  export M4=m4
+  # ayarları onaylama
+  make olddefconfig
+
+  # derleme
+  export LC_ALL=C
+  #make $MAKEJOBS bzImage modules
+  make $MAKEJOBS all
+  make -C tools/bpf/bpftool vmlinux.h feature-clang-bpf-co-re=1
+  set +e
